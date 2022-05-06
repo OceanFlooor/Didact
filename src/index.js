35ca241799903639f1efc59bc60247df98552631
oceanfloor <836173483@qq.com>
2022-05-06 16:52:24 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const TEXT_ELEMENT_TYPE = 'TEXT_ELEMENT';
+
+const createTextElement = (text) => {
+  return {
+    type: TEXT_ELEMENT_TYPE,
+    props: {
+      nodeValue: text,
+      children: [],
+    }
+  }
+}
+
+const createElement = (type, props, ...children) => {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === 'object' ? child : createTextElement(child)
+      )
+    }
+  }
+}
+
+const Didact = {
+  createElement,
+}
+
+/** @jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+)
+
+const container = document.getElementById("root")
+ReactDOM.render(element, container)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
